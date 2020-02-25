47314c83dea2a23f6cfe4ef99a498a669cb44c9a
Satyam Singh <41682264+isaty@users.noreply.github.com>
2020-02-25 11:32:01 +0530

    Update Register.php

--- Register.php
+++ Register.php
@@ -19,7 +19,7 @@ if(!empty($_POST['name'])&&!empty($_POST['email'])&&!empty($_POST['password'])&&
 }
 else
 {
-  echo "fill all the field";
+  echo "fill all the field<script>setimeout(function(){window.location='Registration.html';},3000)</script>";
 }
 
 
